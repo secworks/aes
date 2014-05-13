90a1bd1f9b8d4e305d1c0022b9c3586655981c00
Joachim Strömbergson <joachim@secworks.se>
2014-05-13 11:22:04 +0200

    Adding initial version of rcon implementation.

--- src/rtl/aes_key_mem.v
+++ src/rtl/aes_key_mem.v
@@ -233,7 +233,7 @@ module aes_key_mem(
 
       if (rcon_next)
         begin
-          rcon_new  = ({rcon_reg[6 : 0], 1'b0} ^ (8'h11 & rcon[7]));
+          rcon_new  = ({rcon_reg[6 : 0], 1'b0} ^ (8'h11 & {8{rcon[7]}}));
           rcon_we  = 1;
         end
     end
