71d98011808d94af6927ef3196bc81e790a462d9
Joachim Strömbergson <joachim@secworks.se>
2014-05-14 17:06:33 +0200

    Fixed the functionality of the rcon generator lohgic.

--- src/test/test_rcon.v
+++ src/test/test_rcon.v
@@ -129,7 +129,7 @@ module test_rcon();
 
       if (rcon_next)
         begin
-          rcon_new  = ({rcon_reg[6 : 0], 1'b0} ^ (8'h11 & {8{rcon_reg[7]}}));
+          rcon_new  = {rcon_reg[7 : 0], 1'b0} ^ (9'h11b & {9{rcon_reg[7]}});
           rcon_we  = 1;
         end
     end
