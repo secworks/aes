1884729654c9106bad4a2c22cb4b42ef8362b346
Joachim Strömbergson <joachim@secworks.se>
2014-08-08 20:42:22 +0200

    Removed stuff left from the key expansion test code.

--- src/model/python/aes.py
+++ src/model/python/aes.py
@@ -223,17 +223,6 @@ def next_256it_key_b(key0, key1):
 # key_gen256()
 #
 # Generating the keys for 256 bit keys.
-#
-# Should match:
-#
-#  if i mod 8 = 0:
-#      t = SubWord(RotateWord(W[i-1])) ^ RCon(i/4)
-#      W[i] = t + w[i-8]
-#  else if i mod 4 = 0:
-#      W[i] = SubWord(W[i-1]) + W[i-8]
-#  else
-#      W[i] = W[i-1] ^ W[i-8]
-#
 #-------------------------------------------------------------------
 def key_gen256(key):
     round_keys = []
@@ -255,22 +244,6 @@ def key_gen256(key):
     round_keys.append(k)
 
     return round_keys
-
-
-#-------------------------------------------------------------------
-# key_gen()
-#
-# The actual key generation.
-#-------------------------------------------------------------------
-def key_gen(key):
-    if VERBOSE:
-        print("Generating keys for AES-%d." % (len(key) * 32))
-
-    if (len(key) * 32) == 128:
-        return key_gen128(key)
-
-    else:
-        return key_gen256(key)
 
 
 #-------------------------------------------------------------------
@@ -301,50 +274,6 @@ def print_bytekeys(keys):
             print("0x%02x " % keys[i + j], end="")
         print("")
         i += 16
-        
-
-#-------------------------------------------------------------------
-# test_rcon()
-#
-# Test of the rcon function.
-#-------------------------------------------------------------------
-def test_rcon():
-    print("Testing rcon:")
-    for i in range(20):
-        print("rcon %02d = 0x%02x" % (i, get_rcon(i)))
-
-
-#-------------------------------------------------------------------
-# test_key()
-#
-# Generate round keys for a given key and compare them to
-# the given expected round keys.
-#-------------------------------------------------------------------
-def test_key(key, expected):
-    if len(key) not in [4, 8]:
-        print("Error: Key is %d bits, not 128 or 256 bits" % (len(key) * 32))
-        return
-
-    generated = key_gen(key)
-
-    if (len(generated) != len(expected)):
-        print("Error: Incorrect number of keys generated.")
-        print("Expected number of round keys: %d" % len(expected))
-        print("Got number of round keys:      %d" % len(generated))
-
-    for i in range(len(generated)):
-        exp = expected[i]
-        got = generated[i]
-        if (exp != got):
-            print("Error: Error in round key %d." % i)
-            (e0, e1, e2, e3) = exp
-            (g0, g1, g2, g3) = got
-            print("Expected: 0x%08x 0x%08x 0x%08x 0x%08x"\
-                  % (e0, e1, e2, e3))
-            print("Got:      0x%08x 0x%08x 0x%08x 0x%08x"\
-                  % (g0, g1, g2, g3))
-        else:
-            print("Correct key generated.")
 
 
 #-------------------------------------------------------------------
@@ -383,137 +312,6 @@ def aes_encipher_block(key, block):
 
 
 #-------------------------------------------------------------------
-# test_key_expansion()
-#
-# Perform key expansion tests.
-# The test keys and expected round keys are taken from:
-# http://www.samiam.org/key-schedule.html
-#-------------------------------------------------------------------
-def test_key_expansion():
-    # 128 bit keys.
-    key128_1 = (0x00000000, 0x00000000, 0x00000000, 0x00000000)
-    exp128_1 = ((0x00000000, 0x00000000, 0x00000000, 0x00000000),
-                (0x62636363, 0x62636363, 0x62636363, 0x62636363),
-                (0x9b9898c9, 0xf9fbfbaa, 0x9b9898c9, 0xf9fbfbaa),
-                (0x90973450, 0x696ccffa, 0xf2f45733, 0x0b0fac99),
-                (0xee06da7b, 0x876a1581, 0x759e42b2, 0x7e91ee2b),
-                (0x7f2e2b88, 0xf8443e09, 0x8dda7cbb, 0xf34b9290),
-                (0xec614b85, 0x1425758c, 0x99ff0937, 0x6ab49ba7),
-                (0x21751787, 0x3550620b, 0xacaf6b3c, 0xc61bf09b),
-                (0x0ef90333, 0x3ba96138, 0x97060a04, 0x511dfa9f),
-                (0xb1d4d8e2, 0x8a7db9da, 0x1d7bb3de, 0x4c664941),
-                (0xb4ef5bcb, 0x3e92e211, 0x23e951cf, 0x6f8f188e))
-
-    key128_2 = (0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff)
-    exp128_2 = ((0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff),
-                (0xe8e9e9e9, 0x17161616, 0xe8e9e9e9, 0x17161616),
-                (0xadaeae19, 0xbab8b80f, 0x525151e6, 0x454747f0),
-                (0x090e2277, 0xb3b69a78, 0xe1e7cb9e, 0xa4a08c6e),
-                (0xe16abd3e, 0x52dc2746, 0xb33becd8, 0x179b60b6),
-                (0xe5baf3ce, 0xb766d488, 0x045d3850, 0x13c658e6),
-                (0x71d07db3, 0xc6b6a93b, 0xc2eb916b, 0xd12dc98d),
-                (0xe90d208d, 0x2fbb89b6, 0xed5018dd, 0x3c7dd150),
-                (0x96337366, 0xb988fad0, 0x54d8e20d, 0x68a5335d),
-                (0x8bf03f23, 0x3278c5f3, 0x66a027fe, 0x0e0514a3),
-                (0xd60a3588, 0xe472f07b, 0x82d2d785, 0x8cd7c326))
-
-    key128_3 = (0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f)
-    exp128_3 = ((0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f),
-                (0xd6aa74fd, 0xd2af72fa, 0xdaa678f1, 0xd6ab76fe),
-                (0xb692cf0b, 0x643dbdf1, 0xbe9bc500, 0x6830b3fe),
-                (0xb6ff744e, 0xd2c2c9bf, 0x6c590cbf, 0x0469bf41),
-                (0x47f7f7bc, 0x95353e03, 0xf96c32bc, 0xfd058dfd),
-                (0x3caaa3e8, 0xa99f9deb, 0x50f3af57, 0xadf622aa),
-                (0x5e390f7d, 0xf7a69296, 0xa7553dc1, 0x0aa31f6b),
-                (0x14f9701a, 0xe35fe28c, 0x440adf4d, 0x4ea9c026),
-                (0x47438735, 0xa41c65b9, 0xe016baf4, 0xaebf7ad2),
-                (0x549932d1, 0xf0855768, 0x1093ed9c, 0xbe2c974e),
-                (0x13111d7f, 0xe3944a17, 0xf307a78b, 0x4d2b30c5))
-
-    key128_4 = (0x6920e299, 0xa5202a6d, 0x656e6368, 0x69746f2a)
-    exp128_4 = ((0x6920e299, 0xa5202a6d, 0x656e6368, 0x69746f2a),
-                (0xfa880760, 0x5fa82d0d, 0x3ac64e65, 0x53b2214f),
-                (0xcf75838d, 0x90ddae80, 0xaa1be0e5, 0xf9a9c1aa),
-                (0x180d2f14, 0x88d08194, 0x22cb6171, 0xdb62a0db),
-                (0xbaed96ad, 0x323d1739, 0x10f67648, 0xcb94d693),
-                (0x881b4ab2, 0xba265d8b, 0xaad02bc3, 0x6144fd50),
-                (0xb34f195d, 0x096944d6, 0xa3b96f15, 0xc2fd9245),
-                (0xa7007778, 0xae6933ae, 0x0dd05cbb, 0xcf2dcefe),
-                (0xff8bccf2, 0x51e2ff5c, 0x5c32a3e7, 0x931f6d19),
-                (0x24b7182e, 0x7555e772, 0x29674495, 0xba78298c),
-                (0xae127cda, 0xdb479ba8, 0xf220df3d, 0x4858f6b1))
-
-
-    # 256 bit keys.
-    key256_1 = (0x00000000, 0x00000000, 0x00000000, 0x00000000,
-                0x00000000, 0x00000000, 0x00000000, 0x0000000)
-    exp256_1 = ((0x00000000, 0x00000000, 0x00000000, 0x00000000),
-                (0x00000000, 0x00000000, 0x00000000, 0x00000000),
-                (0x62636363, 0x62636363, 0x62636363, 0x62636363),
-                (0xaafbfbfb, 0xaafbfbfb, 0xaafbfbfb, 0xaafbfbfb),
-                (0x6f6c6ccf, 0x0d0f0fac, 0x6f6c6ccf, 0x0d0f0fac),
-                (0x7d8d8d6a, 0xd7767691, 0x7d8d8d6a, 0xd7767691),
-                (0x5354edc1, 0x5e5be26d, 0x31378ea2, 0x3c38810e),
-                (0x968a81c1, 0x41fcf750, 0x3c717a3a, 0xeb070cab),
-                (0x9eaa8f28, 0xc0f16d45, 0xf1c6e3e7, 0xcdfe62e9),
-                (0x2b312bdf, 0x6acddc8f, 0x56bca6b5, 0xbdbbaa1e),
-                (0x6406fd52, 0xa4f79017, 0x553173f0, 0x98cf1119),
-                (0x6dbba90b, 0x07767584, 0x51cad331, 0xec71792f),
-                (0xe7b0e89c, 0x4347788b, 0x16760b7b, 0x8eb91a62),
-                (0x74ed0ba1, 0x739b7e25, 0x2251ad14, 0xce20d43b),
-                (0x10f80a17, 0x53bf729c, 0x45c979e7, 0xcb706385))
-
-    key256_2 = (0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-                0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff)
-    exp256_2 = ((0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff),
-                (0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff),
-                (0xe8e9e9e9, 0x17161616, 0xe8e9e9e9, 0x17161616),
-                (0x0fb8b8b8, 0xf0474747, 0x0fb8b8b8, 0xf0474747),
-                (0x4a494965, 0x5d5f5f73, 0xb5b6b69a, 0xa2a0a08c),
-                (0x355858dc, 0xc51f1f9b, 0xcaa7a723, 0x3ae0e064),
-                (0xafa80ae5, 0xf2f75596, 0x4741e30c, 0xe5e14380),
-                (0xeca04211, 0x29bf5d8a, 0xe318faa9, 0xd9f81acd),
-                (0xe60ab7d0, 0x14fde246, 0x53bc014a, 0xb65d42ca),
-                (0xa2ec6e65, 0x8b5333ef, 0x684bc946, 0xb1b3d38b),
-                (0x9b6c8a18, 0x8f91685e, 0xdc2d6914, 0x6a702bde),
-                (0xa0bd9f78, 0x2beeac97, 0x43a565d1, 0xf216b65a),
-                (0xfc223491, 0x73b35ccf, 0xaf9e35db, 0xc5ee1e05),
-                (0x0695ed13, 0x2d7b4184, 0x6ede2455, 0x9cc8920f),
-                (0x546d424f, 0x27de1e80, 0x88402b5b, 0x4dae355e))
-
-    key256_3 = (0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f,
-                0x10111213, 0x14151617, 0x18191a1b, 0x1c1d1e1f)
-    exp256_3 = ((0x00010203, 0x04050607, 0x08090a0b, 0x0c0d0e0f),
-                (0x10111213, 0x14151617, 0x18191a1b, 0x1c1d1e1f),
-                (0xa573c29f, 0xa176c498, 0xa97fce93, 0xa572c09c),
-                (0x1651a8cd, 0x0244beda, 0x1a5da4c1, 0x0640bade),
-                (0xae87dff0, 0x0ff11b68, 0xa68ed5fb, 0x03fc1567),
-                (0x6de1f148, 0x6fa54f92, 0x75f8eb53, 0x73b8518d),
-                (0xc656827f, 0xc9a79917, 0x6f294cec, 0x6cd5598b),
-                (0x3de23a75, 0x524775e7, 0x27bf9eb4, 0x5407cf39),
-                (0x0bdc905f, 0xc27b0948, 0xad5245a4, 0xc1871c2f),
-                (0x45f5a660, 0x17b2d387, 0x300d4d33, 0x640a820a),
-                (0x7ccff71c, 0xbeb4fe54, 0x13e6bbf0, 0xd261a7df),
-                (0xf01afafe, 0xe7a82979, 0xd7a5644a, 0xb3afe640),
-                (0x2541fe71, 0x9bf50025, 0x8813bbd5, 0x5a721c0a),
-                (0x4e5a6699, 0xa9f24fe0, 0x7e572baa, 0xcdf8cdea),
-                (0x24fc79cc, 0xbf0979e9, 0x371ac23c, 0x6d68de36))
-
-    print("*** Test of 128 bit keys: ***")
-    test_key(key128_1, exp128_1)
-    test_key(key128_2, exp128_2)
-    test_key(key128_3, exp128_3)
-    test_key(key128_4, exp128_4)
-    print("")
-
-    print("*** Test of 256 bit keys: ***")
-    test_key(key256_1, exp256_1)
-    test_key(key256_2, exp256_2)
-    test_key(key256_3, exp256_3)
-    print("")
-
-
-#-------------------------------------------------------------------
 # main()
 #
 # If executed tests the ChaCha class using known test vectors.
@@ -522,8 +320,6 @@ def main():
     print("Testing the AES cipher model")
     print("============================")
     print
-
-    test_key_expansion()
 
 
 #-------------------------------------------------------------------
