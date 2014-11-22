ac40c0c1032d03c223218399a107feeae5a73a5d
Joachim Strömbergson <joachim@secworks.se>
2014-11-22 10:21:40 +0100

    Adding mixcolumns function and rearranging all functions to have the subfunctions closer to the main functions.

--- src/rtl/aes_encipher_block.v
+++ src/rtl/aes_encipher_block.v
@@ -80,7 +80,7 @@ module aes_encipher_block(
 
 
   //----------------------------------------------------------------
-  // Gaolis multiplication functions for MixColumn.
+  // Round functions with sub functions.
   //----------------------------------------------------------------
   function [7 : 0] gm2(input [7 : 0] op);
     begin
@@ -94,28 +94,23 @@ module aes_encipher_block(
     end
   endfunction // gm3
 
-
-  //----------------------------------------------------------------
-  // Round functions.
-  //----------------------------------------------------------------
-  function [127 : 0] shiftrows(input [127 : 0] data);
-    reg [31 : 0] w0, w1, w2, w3;
-    reg [31 : 0] ws0, ws1, ws2, ws3;
+  function [31 : 0] mixw(input [31 : 0] w);
+    reg [7 : 0] b0, b1, b2, b3;
+    reg [7 : 0] mb0, mb1, mb2, mb3;
     begin
-      w0 = data[127 : 096];
-      w1 = data[095 : 064];
-      w1 = data[063 : 032];
-      w2 = data[031 : 000];
+      b0 = w[31 : 24];
+      b1 = w[23 : 16];
+      b2 = w[15 : 08];
+      b3 = w[07 : 00];
 
-      ws0 = {w0[31 : 24], w1[23 : 16], w2[15 : 08], w3[07 : 00]};
-      ws1 = {w1[31 : 24], w2[23 : 16], w3[15 : 08], w0[07 : 00]};
-      ws2 = {w2[31 : 24], w3[23 : 16], w0[15 : 08], w1[07 : 00]};
-      ws3 = {w3[31 : 24], w0[23 : 16], w1[15 : 08], w2[07 : 00]};
+      mb0 = gm2(b0) ^ gm3(b1) ^ b2      ^ b3;
+      mb1 = b0      ^ gm2(b1) ^ gm3(b2) ^ b3;
+      mb2 = b0      ^ b1      ^ gm2(b2) ^ gm3(b3);
+      mb3 = gm3(b0) ^ b1      ^ b2      ^ gm2(b3);
 
-      shiftrows = {ws0, ws1, ws2, ws3};
+      mixw = {mb0, mb1, mb2, mb3};
     end
-  endfunction // shiftrows
-
+  endfunction // mixw
 
   function [127 : 0] mixcolumns(input [127 : 0] data);
     reg [31 : 0] w0, w1, w2, w3;
@@ -135,6 +130,23 @@ module aes_encipher_block(
     end
   endfunction // mixcolumns
 
+  function [127 : 0] shiftrows(input [127 : 0] data);
+    reg [31 : 0] w0, w1, w2, w3;
+    reg [31 : 0] ws0, ws1, ws2, ws3;
+    begin
+      w0 = data[127 : 096];
+      w1 = data[095 : 064];
+      w1 = data[063 : 032];
+      w2 = data[031 : 000];
+
+      ws0 = {w0[31 : 24], w1[23 : 16], w2[15 : 08], w3[07 : 00]};
+      ws1 = {w1[31 : 24], w2[23 : 16], w3[15 : 08], w0[07 : 00]};
+      ws2 = {w2[31 : 24], w3[23 : 16], w0[15 : 08], w1[07 : 00]};
+      ws3 = {w3[31 : 24], w0[23 : 16], w1[15 : 08], w2[07 : 00]};
+
+      shiftrows = {ws0, ws1, ws2, ws3};
+    end
+  endfunction // shiftrows
 
   function [127 : 0] addroundkey(input [127 : 0] data, input [127 : 0] key);
     begin
